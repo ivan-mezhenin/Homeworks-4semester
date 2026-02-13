ca29ca88bb32af70668f0840fa920a70798004d6
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-02-13 08:41:50 +0300

    feat: factorial and tests

---
+++ Homework1/Task1/Factorial.fs
@@ -0,0 +1,11 @@
+﻿module Factorial
+
+let factorial n  = 
+    let rec loop n acc =  
+        match n with
+        | 0 | 1 -> Some acc
+        | n when n < 0 -> None
+        | _ -> loop (n - 1) (acc * n)
+    
+    loop n 1
+
