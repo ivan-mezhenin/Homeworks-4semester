fcc4d876fa3004bcb67d7fabfd1d379a0ca9fa02
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-03-11 14:56:09 +0300

    feat: Lambda and FV types

---
+++ Homework3/Task1/lambda.fs
@@ -0,0 +1,17 @@
+﻿module lambda
+
+type Lambda = 
+    | Var of string
+    | App of Lambda * Lambda
+    | Abs of string * Lambda
+
+type rec FV = function
+    | Var x -> Set.singleton x
+    | App (a,b) -> Set.union (FV x) (FV y) 
+    | Abs (x, F) -> Set.remove x (FV F)
+
+let rec newVarName usedNames currentVarName =
+    let rec generateNewName n =
+        let name = if n = 0 then currentVarName else sprintf "%s%d" basename n
+        if Set.contains name usedNames then generateNewName (n + 1) else name
+    generateNewName 0
