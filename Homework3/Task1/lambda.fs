493b423ef4e025d35646dd9f7b1a188d26393f70
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-03-12 18:56:44 +0300

    feat: add substitute

--- Homework3/Task1/lambda.fs
+++ Homework3/Task1/lambda.fs
@@ -1,17 +1,41 @@
 ﻿module lambda
 
-type Lambda = 
+type lambda = 
     | Var of string
-    | App of Lambda * Lambda
-    | Abs of string * Lambda
+    | App of lambda * lambda
+    | Abs of string * lambda
 
-type rec FV = function
+let rec FV = function
     | Var x -> Set.singleton x
-    | App (a,b) -> Set.union (FV x) (FV y) 
-    | Abs (x, F) -> Set.remove x (FV F)
+    | App (a,b) -> Set.union (FV a) (FV b) 
+    | Abs (x, f) -> Set.remove x (FV f)
 
 let rec newVarName usedNames currentVarName =
     let rec generateNewName n =
-        let name = if n = 0 then currentVarName else sprintf "%s%d" basename n
+        let name = if n = 0 then currentVarName else sprintf "%s%d" currentVarName n
         if Set.contains name usedNames then generateNewName (n + 1) else name
     generateNewName 0
+
+let rec substitute variableToReplace expressionToInsert targetExpression =
+    let rec substInner BV currentExpr =
+        match currentExpr with
+        | Var y when y = variableToReplace -> 
+            if Set.contains y BV then 
+                failwith "Should not happen due to alpha conversion"
+            else expressionToInsert
+        | Var y -> Var y
+        | App (e1, e2) -> App (substInner BV e1, substInner BV e2)
+        | Abs (x, body) ->
+            if x = variableToReplace then
+                Abs (x, body)
+            else
+                let freeInWith = FV expressionToInsert
+                if Set.contains x freeInWith then
+                    let allVars = Set.union (FV currentExpr) (FV expressionToInsert)
+                    let newName = newVarName allVars x
+                    let newBody = substInner (Set.add newName BV) (substitute x (Var newName) body)
+                    Abs (newName, substInner BV newBody)
+                else
+                    Abs (x, substInner (BV) body)
+    substInner Set.empty targetExpression
+
