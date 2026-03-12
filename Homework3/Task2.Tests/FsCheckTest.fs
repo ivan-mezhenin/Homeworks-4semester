95c2982a7e082076f329ff788c6b7d80cf560530
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-03-12 20:47:57 +0300

    test: add fsCheck test

---
+++ Homework3/Task2.Tests/FsCheckTest.fs
@@ -0,0 +1,18 @@
+﻿namespace MyProject.Tests
+
+open Xunit
+open FsCheck
+open FsCheck.Xunit
+open PointFree
+
+module PointFreeTests =
+
+    [<Property>]
+    let ``Все варианты func дают одинаковый результат`` (x: int) (l: int list) =
+        let expected = func1 x l
+        
+        let r1 = func2 x l
+        let r2 = func3 x l
+        let r3 = func4  x l
+        
+        expected = r1 && r1 = r2 && r2 = r3
