a17c2c0dca0a78c2e3e43d7278df20a37726916d
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-04-02 22:19:35 +0300

    feat: solution with tests:

---
+++ Homework2/ParsingTree/ParsingTree.fs
@@ -0,0 +1,21 @@
+﻿module ParsingTree
+
+type Expr =
+    | Number of int
+    | Negative of Expr
+    | Add of Expr * Expr
+    | Sub of Expr * Expr
+    | Mul of Expr * Expr
+    | Div of Expr * Expr
+
+let rec evaluate (expr: Expr) : int =
+    match expr with
+    | Number n          -> n
+    | Negative e          -> - (evaluate e)
+    | Add (a, b)     -> evaluate a + evaluate b
+    | Sub (a, b)     -> evaluate a - evaluate b
+    | Mul (a, b)     -> evaluate a * evaluate b
+    | Div (a, b)     ->
+        let divisor = evaluate b
+        if divisor = 0 then failwith "Division by zero"
+        evaluate a / divisor
