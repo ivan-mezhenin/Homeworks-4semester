85b88c7fe643b5ee911f22910a9c56d3a2898d7e
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-04-02 21:12:02 +0300

    fix

--- Homework1/Task2.Tests/NumberOfDegreesTests.fs
+++ Homework1/Task2.Tests/NumberOfDegreesTests.fs
@@ -4,28 +4,51 @@ open NUnit.Framework
 open FsUnit
 open NumberOfDegrees
 
-[<TestCase(0, 0, [|1|])>]
-[<TestCase(1, 0, [|2|])>]
-[<TestCase(2, 0, [|4|])>]
-[<TestCase(3, 0, [|8|])>]
-[<TestCase(0, 1, [|1;2|])>]
-[<TestCase(0, 2, [|1;2;4|])>]
-[<TestCase(0, 3, [|1;2;4;8|])>]
-[<TestCase(1, 1, [|2;4|])>]
-[<TestCase(1, 2, [|2;4;8|])>]
-[<TestCase(1, 3, [|2;4;8;16|])>]
-[<TestCase(2, 2, [|4;8;16|])>]
-[<TestCase(2, 3, [|4;8;16;32|])>]
-[<TestCase(3, 3, [|8;16;32;64|])>]
-let ``degrees should return correct powers of two`` (n, m, expected) =
-    degrees n m |> should equal (List.ofArray expected)
+let shouldEqualFloat (expected: float) (actual: float) =
+    actual |> should (equalWithin 1e-10) expected
+
+let shouldEqualFloatList (expected: float list) (actual: float list) =
+    actual |> should haveLength expected.Length
+    List.zip expected actual
+    |> List.iter (fun (exp, act) -> shouldEqualFloat exp act)
+
+[<TestCase(0, 0, [|1.0|])>]
+[<TestCase(1, 0, [|2.0|])>]
+[<TestCase(2, 0, [|4.0|])>]
+[<TestCase(3, 0, [|8.0|])>]
+[<TestCase(0, 1, [|1.0; 2.0|])>]
+[<TestCase(0, 2, [|1.0; 2.0; 4.0|])>]
+[<TestCase(0, 3, [|1.0; 2.0; 4.0; 8.0|])>]
+[<TestCase(1, 1, [|2.0; 4.0|])>]
+[<TestCase(1, 2, [|2.0; 4.0; 8.0|])>]
+[<TestCase(1, 3, [|2.0; 4.0; 8.0; 16.0|])>]
+let ``degrees should return correct powers of two`` (n, m, expected: float[]) =
+    let result = degrees n m
+    shouldEqualFloatList (List.ofArray expected) result
 
 [<Test>]
-let ``degrees with m = 0 returns single element list`` () =
-    degrees 5 0 |> should equal [32]
-    degrees 10 0 |> should equal [1024]
+let ``degrees with negative n works correctly`` () =
+    degrees (-3) 2 |> shouldEqualFloatList [0.125; 0.25; 0.5]
+    degrees (-1) 3 |> shouldEqualFloatList [0.5; 1.0; 2.0; 4.0]
+    degrees (-2) 0 |> shouldEqualFloatList [0.25]
 
 [<Test>]
-let ``degrees with n = 0 returns powers from 1`` () =
-    degrees 0 3 |> should equal [1;2;4;8]
-    degrees 0 5 |> should equal [1;2;4;8;16;32]
+let ``degrees with m = 0 returns single element`` () =
+    degrees 5 0  |> shouldEqualFloatList [32.0]
+    degrees (-4) 0 |> shouldEqualFloatList [0.0625]
+
+[<Test>]
+let ``degrees returns empty list when m is negative`` () =
+    degrees 5 (-1) |> should be Empty
+    degrees (-3) (-2) |> should be Empty
+    degrees 0 (-5) |> should be Empty
+    degrees 10 (-1) |> should be Empty
+
+[<Test>]
+let ``degrees with n = 0 returns correct sequence`` () =
+    degrees 0 4 |> shouldEqualFloatList [1.0; 2.0; 4.0; 8.0; 16.0]
+
+[<Test>]
+let ``degrees handles large positive n without overflow`` () =
+    degrees 20 5 |> shouldEqualFloatList 
+        [1048576.0; 2097152.0; 4194304.0; 8388608.0; 16777216.0; 33554432.0]
--- Homework1/Task2/NumberOfDegrees.fs
+++ Homework1/Task2/NumberOfDegrees.fs
@@ -1,9 +1,11 @@
 ﻿module NumberOfDegrees
 
-let degrees n m = 
-    let rec lstOfDegress current last lst acc =
-        match current with
-        |_ when current = last -> (acc :: lst) |> List.rev
-        |_ -> lstOfDegress (current + 1) last (acc :: lst) (acc * 2) 
+let degrees n m =
+    if m < 0 then []
+    else
+        let rec lstOfDegress current last lst acc =
+            match current with
+            | c when c > last -> List.rev lst
+            | _ -> lstOfDegress (current + 1) last (acc :: lst) (acc * 2.0)
 
-    lstOfDegress n (n + m) [] (pown 2 n)
+        lstOfDegress n (n + m) [] (2.0 ** float n)
